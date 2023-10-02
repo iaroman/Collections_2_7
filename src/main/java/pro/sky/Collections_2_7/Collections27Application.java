8a8bf3820eb220e6e9961c8cefd8da23aea12773
Роман <roma_yakushin@mail.ru>
2023-10-02 23:48:38 +0300

    Start

---
+++ src/main/java/pro/sky/Collections_2_7/Collections27Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Collections_2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collections27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Collections27Application.class, args);
+	}
+
+}
