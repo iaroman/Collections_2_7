71f7af47ec12be303742bc49edaf3ff5eaf736ea
Роман <roma_yakushin@mail.ru>
2023-10-02 23:48:20 +0300

    Start

---
+++ src/test/java/pro/sky/Collections_2_7/Collections27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Collections_2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collections27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
